add87a0536b8210d9738066c73ffc8f9f457bfe4
Iván Beltrán G <ivanbeltrangonzalez@hotmail.com>
2026-03-11 20:32:54 -0500

    Agregando archivo de tablas

--- ciclofor.py
+++ ciclofor.py
@@ -1,18 +1,21 @@
 #-------------------------
-"""for x in range(1, 11):
+"""for x in range(1, 11): # este me imprime del 1 al 11-1 osea 10
     print(x)"""
 #-------------------------
-"""for x in range(5):
+"""for x in range(5):  # este me imprime del 0 al 4
     print(x)"""
 #-------------------------
-"""for x in range(1, 11, 2):
+"""for x in range(1, 11, 2): # este me imprime del 1 a 11-1 osea 10 de 2 en 2
     print(x)"""
 #-------------------------
 """texto = "Python"
-for letra in texto:
+for letra in texto:    # este toma el texto e imprime cada letra 
     print(letra)"""
 #-------------------------
-
+"""
 num = int(input("Digite el numero de la tabla que desea: "))
-for x in range(1, 11):
-    print(f"{x} x {num} = ",(x*num))
+for x in range(1, 11):                                          # este imprime la tabla de multiplicar escogida
+    print(f"{num} x {x} = ",(x*num))
+
+"""
+#-------------------------
---
+++ tablas.py
@@ -0,0 +1,6 @@
+for num in range(1, 11):
+    print("")                           # este codigo imprime las tablas del 1 al 10 de manera secuencial
+    print(f"Tabla del {num}")
+    for x in range(1, 11):
+       print(f"{num} x {x} = ",(x*num))
+        
